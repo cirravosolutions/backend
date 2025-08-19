bbc02204f4078bc2260e547878eb58f4c10fa4a0
cirravosolutions <cirravosolutions@gmail.com>
2025-08-19 14:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// ✅ Allow all origins (for testing)
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Render backend 🚀" });
